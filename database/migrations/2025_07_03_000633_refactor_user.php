ef587e557f44ebac8dabb8bdce5d64f335c9f30f
Malsoryz <ikmalalansory@gmail.com>
2025-07-03 11:35:55 +0800

    refactor user tables

---
+++ database/migrations/2025_07_03_000633_refactor_user.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->after('password', function (Blueprint $table) {
+                $table->text('address');
+                $table->date('birth_date');
+                $table->enum('gender', ['male', 'female']);
+                $table->string('phone_number');
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
