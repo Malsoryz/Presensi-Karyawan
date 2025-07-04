861d7f565e2885b216e892d73ff00c7a7bf81259
Malsoryz <ikmalalansory@gmail.com>
2025-07-04 11:06:46 +0800

    membuat halaman menjadi 2, scan dan terscan

---
+++ resources/views/QRCodePresence/index.blade.php
@@ -0,0 +1,49 @@
+@extends('QRCodePresenceLayout')
+
+@section('main-content')
+
+<div class="flex gap-16">
+    <div class="flex gap-16 flex-col">
+        <div class="p-2 rounded-md bg-white">
+            {!! QrCode::size(256)->generate(route('presensi.qr.store', ['token' => $token])) !!}
+        </div>
+        <div>
+            <span>
+                “ Nothing worth having comes easy. ” <br>
+                — Theodore Roosevelt <br>
+                {{ route('presensi.qr.store', ['token' => $token]) }}
+            </span>
+        </div>
+    </div>
+    <div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+    <table class="table">
+        <!-- head -->
+        <thead>
+            <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Job</th>
+                <th>Favorite Color</th>
+                <th>Bla</th>
+                <th>Bla</th>
+                <th>Bla</th>
+            </tr>
+        </thead>
+        <tbody>
+            @for ($i = 0; $i < 5; $i++)
+                <tr>
+                    <th>{{ $i }}</th>
+                    <td>Dummy</td>
+                    <td>Anything</td>
+                    <td>Anything</td>
+                    <td>Anything</td>
+                    <td>Anything</td>
+                    <td>Anything</td>
+                </tr>
+            @endfor
+        </tbody>
+    </table>
+    </div>
+</div>
+
+@endsection
