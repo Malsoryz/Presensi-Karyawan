21958f9d57f79533137e60450c981932a4d220d1
Malsoryz <ikmalalansory@gmail.com>
2025-07-16 16:55:14 +0800

    add new method 'message()' for retrieving message for info view

--- app/Enums/SesiPresensi.php
+++ app/Enums/SesiPresensi.php
@@ -7,4 +7,14 @@ enum SesiPresensi: string {
     case BELUM_MULAI = 'belum mulai';
     case SESI_PRESENSI = 'sesi presensi';
     case SELESAI = 'selesai';
+
+    public function message(): string
+    {
+        return match ($this) {
+            SesiPresensi::LIBUR => 'Tidak ada presensi hari ini karena libur.',
+            SesiPresensi::BELUM_MULAI => 'Sesi presensi belum dimulai.',
+            SesiPresensi::SESI_PRESENSI => 'Sesi presensi sedang berlangsung.',
+            SesiPresensi::SELESAI => 'Sesi presensi telah selesai.',
+        };
+    }
 }
--- app/Enums/StatusPresensi.php
+++ app/Enums/StatusPresensi.php
@@ -9,4 +9,16 @@ enum StatusPresensi: string {
     case IJIN = 'ijin';
     case SAKIT = 'sakit';
     case TIDAK_MASUK = 'tidak_masuk';
+
+    public function message(): string
+    {
+        return match ($this) {
+            StatusPresensi::BELUM => 'Anda belum melakukan presensi.',
+            StatusPresensi::MASUK => 'Anda telah melakukan presensi.',
+            StatusPresensi::TERLAMBAT => 'Anda telah melakukan presensi walaupun anda terlambat.',
+            StatusPresensi::IJIN => 'Anda telah diberikan ijin untuk tidak masuk kerja.',
+            StatusPresensi::SAKIT => 'Anda telah diberikan ijin untuk tidak masuk kerja karena sakit.',
+            StatusPresensi::TIDAK_MASUK => 'Anda tidak melakukan presensi dan tidak masuk kerja. kenapa?',
+        };
+    }
 }
