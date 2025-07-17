01705250a39ba827c0986a4dee4e8c553696db8c
Malsoryz <ikmalalansory@gmail.com>
2025-07-17 11:43:34 +0800

    add new method 'icon()' for displaying icon in info view and some changes at 'message()' method

--- app/Enums/SesiPresensi.php
+++ app/Enums/SesiPresensi.php
@@ -2,6 +2,9 @@
 
 namespace App\Enums;
 
+use App\Models\HariLibur as HL;
+use App\Models\Config as CFG;
+
 enum SesiPresensi: string {
     case LIBUR = 'libur';
     case BELUM_MULAI = 'belum mulai';
@@ -10,8 +13,16 @@ enum SesiPresensi: string {
 
     public function message(): string
     {
+        $timezone = CFG::get('timezone', 'Asia/Makassar');
+        $today = now($timezone)->toDateString();
+        $hariLibur = HL::whereDate('tanggal', $today)->first();
+        $isHariLibur = HL::whereDate('tanggal', $today)->exists();
+        $liburMessage = $isHariLibur ? 
+            "Tidak ada presensi hari ini karena libur <strong>$hariLibur->nama</strong>":
+            "Tidak ada presensi hari ini karena hari <strong>minggu.</strong>";
+
         return match ($this) {
-            SesiPresensi::LIBUR => 'Tidak ada presensi hari ini karena libur.',
+            SesiPresensi::LIBUR => $liburMessage,
             SesiPresensi::BELUM_MULAI => 'Sesi presensi belum dimulai.',
             SesiPresensi::SESI_PRESENSI => 'Sesi presensi sedang berlangsung.',
             SesiPresensi::SELESAI => 'Sesi presensi telah selesai.',
--- app/Enums/StatusPresensi.php
+++ app/Enums/StatusPresensi.php
@@ -2,6 +2,8 @@
 
 namespace App\Enums;
 
+use Illuminate\View\ComponentAttributeBag;
+
 enum StatusPresensi: string {
     case BELUM = 'belum';
     case MASUK = 'masuk';
@@ -19,6 +21,26 @@ enum StatusPresensi: string {
             StatusPresensi::IJIN => 'Anda telah diberikan ijin untuk tidak masuk kerja.',
             StatusPresensi::SAKIT => 'Anda telah diberikan ijin untuk tidak masuk kerja karena sakit.',
             StatusPresensi::TIDAK_MASUK => 'Anda tidak melakukan presensi dan tidak masuk kerja. kenapa?',
+        };
+    }
+
+    public function icon($extraClass = null): string
+    {
+        $defaultClass = 'w-24 h-24';
+        $class = "$defaultClass $extraClass";
+        return match ($this) {
+            StatusPresensi::MASUK, 
+            StatusPresensi::TERLAMBAT => view('e60dd9d2c3a62d619c9acb38f20d5aa5::icon.check-circle', [
+                'attributes' => new ComponentAttributeBag(['class' => "text-green-400 $class"])
+            ])->render(),
+            StatusPresensi::BELUM, 
+            StatusPresensi::IJIN, 
+            StatusPresensi::SAKIT => view('e60dd9d2c3a62d619c9acb38f20d5aa5::icon.information-circle', [
+                'attributes' => new ComponentAttributeBag(['class' => "text-blue-400 $class"])
+            ])->render(),
+            StatusPresensi::TIDAK_MASUK => view('e60dd9d2c3a62d619c9acb38f20d5aa5::icon.exclamation-circle', [
+                'attributes' => new ComponentAttributeBag(['class' => "text-red-400 $class"])
+            ])->render(),
         };
     }
 }
