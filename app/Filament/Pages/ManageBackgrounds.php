b1920c9f5339fa4f9311a32b0a74d74745151a23
Malsoryz <ikmalalansory@gmail.com>
2025-07-17 22:49:30 +0800

    update page for managing backgrounds

--- app/Filament/Pages/ManageBackgrounds.php
+++ app/Filament/Pages/ManageBackgrounds.php
@@ -3,12 +3,19 @@
 namespace App\Filament\Pages;
 
 use App\Models\Background;
+use App\Models\Config;
+
+use Illuminate\Support\Str;
 
 use Filament\Pages\Page;
+use Filament\Forms\Get;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Actions\Action;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
+
+use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
 
 class ManageBackgrounds extends Page implements HasForms
 {
@@ -27,7 +34,33 @@ class ManageBackgrounds extends Page implements HasForms
                     ->label('Background Name')
                     ->required()
                     ->maxLength(255),
-            ]);
+                FileUpload::make('image_path')
+                    ->label('Background Image')
+                    ->required()
+                    ->image()
+                    ->disk('public')
+                    ->directory('backgrounds')
+                    ->visibility('public')
+                    ->maxSize(2048)
+                    ->getUploadedFileNameForStorageUsing(function (TemporaryUploadedFile $file, Get $get): string{
+                        $name = $get('name') ?? 'background';
+                        $slug = Str::slug($name, '-');
+                        $timestamp = now(Config::get('timezone', 'Asia/Makassar'))->format('YmdHis');
+                        $extension = $file->getClientOriginalExtension();
+                        return "{$slug}-{$timestamp}.{$extension}";
+                    }),
+            ])
+            ->action(function (array $data, Background $background) {
+                $background->create($data);
+                $this->dispatch('backgroundAdded', [
+                    'message' => 'Background added successfully!',
+                ]);
+            });
+    }
+
+    public function getListOfBackgrounds()
+    {
+        return Background::all();
     }
 
     public function getHeaderActions(): array
--- resources/views/filament/pages/manage-backgrounds.blade.php
+++ resources/views/filament/pages/manage-backgrounds.blade.php
@@ -1,4 +1,16 @@
 <x-filament-panels::page>
+    <x-filament::section>
+        <x-slot name="heading">
+            Saved backgrounds
+        </x-slot>
+
+        @foreach ($this->getListOfBackgrounds() as $background)
+            <div>
+                <img src="{{ asset('storage/'.$background->image_path) }}" alt="Background Image" class="w-full h-64 object-cover mb-4">
+            </div>
+        @endforeach
+
+    </x-filament::section>
 
     <x-filament-actions::modals/>
 </x-filament-panels::page>
