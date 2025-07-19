4f3ac409a5c356a4b62d383e58c23a31a01a5614
Malsoryz <ikmalalansory@gmail.com>
2025-07-19 10:24:37 +0800

    update background manager page

--- app/Filament/Pages/ManageBackgrounds.php
+++ app/Filament/Pages/ManageBackgrounds.php
@@ -18,14 +18,6 @@ use Filament\Actions\Action;
 use Filament\Actions\Concerns\InteractsWithActions;
 use Filament\Actions\Contracts\HasActions;
 
-use Filament\Tables\Table;
-use Filament\Tables\Actions\Action as TableAction;
-use Filament\Tables\Concerns\InteractsWithTable;
-use Filament\Tables\Contracts\HasTable;
-use Filament\Tables\Columns\Layout\Stack;
-use Filament\Tables\Columns\ImageColumn;
-use Filament\Tables\Columns\ViewColumn;
-
 use Filament\Forms\Get;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
@@ -37,43 +29,23 @@ use Illuminate\Database\Eloquent\Model;
 
 use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
 
-class ManageBackgrounds extends Page implements HasForms, HasTable, HasActions
+class ManageBackgrounds extends Page implements HasForms, HasActions
 {
     use InteractsWithForms;
-    use InteractsWithTable;
     use InteractsWithActions;
 
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.pages.manage-backgrounds';
 
-    public function table(Table $table): Table
+    public ?int $selectedId = null;
+
+    public ?Maxwidth $viewModalWidth = MaxWidth::FiveExtraLarge;
+
+    public function openViewModal(int $id)
     {
-        return $table
-            ->heading('Backgrounds')
-            ->description('List dari background yang tersimpan.')
-            ->paginated(false)
-            ->query(Background::query())
-            ->columns([
-                Stack::make([
-                    ViewColumn::make('image_path')
-                        ->view('filament.components.tables.columns.image-column')
-                        ->action(
-                            TableAction::make('view')
-                                ->modalSubmitAction(false)
-                                ->modalHeading(fn (Model $record) => 'View ('.$record->name.')')
-                                ->modalContent(function (Model $record) {
-                                    return view('filament.components.tables.actions.image-modal-content', [
-                                        'imagePath' => $record->image_path,
-                                        'name' => $record->name,
-                                    ]);
-                                }),
-                        ),
-                ]),
-            ])
-            ->contentGrid([
-                'default' => 4,
-            ]);
+        $this->selectedId = $id;
+        $this->dispatch('open-modal', id: 'view-modal');
     }
 
     public function createAction(): Action
@@ -111,33 +83,47 @@ class ManageBackgrounds extends Page implements HasForms, HasTable, HasActions
             });
     }
 
-    public function deleteAction(Background $data): Action
+    public function deleteAction(): Action
     {
         return Action::make('delete')
-            ->label('Delete')
-            ->icon('heroicon-o-trash')
-            ->color('danger')
-            ->requiresConfirmation()
-            ->modalIcon('heroicon-o-trash')
-            ->modalHeading('Delete background?')
-            ->modalDescription('Are you sure you\'d like to delete this background? This cannot be undone.')
-            ->action(function () {
-                if ($data->image_path && Storage::disk('public')->exists($data->image_path)) {
-                    Storage::disk('public')->delete($data->image_path);
+        ->label('Delete')
+        ->icon('heroicon-o-trash')
+        ->color('danger')
+        ->requiresConfirmation()
+        ->modalIcon('heroicon-o-trash')
+        ->modalHeading('Delete background?')
+        ->modalDescription('Are you sure you\'d like to delete this background? This cannot be undone.')
+        ->action(function () {
+                $background = Background::find($this->selectedId);
+                if ($background->image_path && Storage::disk('public')->exists($background->image_path)) {
+                    Storage::disk('public')->delete($background->image_path);
                 }
 
-                Background::where('name', $data->name)->delete();
+                Background::where('name', $background->name)->delete();
 
                 Notification::make()
                     ->title('Deleted successfully.')
                     ->success()
                     ->send();
+
+                    $this->selectedId = null;
+                    $this->dispatch('close-modal', id: 'view-modal');
             });
     }
 
     public function getListOfBackgrounds()
     {
         return Background::all();
+    }
+
+    public function getBackgrounds($isSpecial = false)
+    {
+        return Background::where('special_friday', $isSpecial)->get();
+    }
+
+    public function findBackground($id)
+    {
+        return Background::find($id);
     }
 
     public function getHeaderActions(): array
--- resources/views/filament/pages/manage-backgrounds.blade.php
+++ resources/views/filament/pages/manage-backgrounds.blade.php
@@ -1,5 +1,95 @@
 <x-filament-panels::page>
-    {{ $this->table }}
+
+    <x-filament::section collapsible>
+        <x-slot name="heading">
+            Backgrounds
+        </x-slot>
+        <x-slot name="description">
+            List dari background yang tersimpan.
+        </x-slot>
+
+        <x-filament::grid default="{{ $this->getBackgrounds()->isEmpty() ? 1 : 4 }}" direction="column">
+            @if ($this->getBackgrounds()->isEmpty())
+                <div class="w-full h-auto flex flex-col justify-center items-center">
+                    <x-filament::icon
+                        icon="heroicon-o-photo"
+                        class="text-gray-700 h-32 w-32"
+                    />
+                    <span class="text-gray-700">
+                        Tidak ada background.
+                    </span>
+                </div>
+            @else
+                @foreach ($this->getBackgrounds() as $background)
+                    <x-filament::grid.column>
+                        <img 
+                            src="{{ asset('storage/'.$background->image_path) }}" 
+                            alt="{{ $background->name }}"
+                            wire:click="openViewModal({{ $background->id }})"
+                        >
+                    </x-filament::grid.column>
+                @endforeach
+            @endif
+        </x-filament::grid>
+    </x-filament::section>
+
+    <x-filament::section collapsible>
+        <x-slot name="heading">
+            Special Friday Backgrounds
+        </x-slot>
+        <x-slot name="description">
+            Background spesial untuk jum'at.
+        </x-slot>
+
+        <x-filament::grid default="{{ $this->getBackgrounds(true)->isEmpty() ? 1 : 4 }}" direction="column">
+            @if ($this->getBackgrounds(true)->isEmpty())
+                <div class="w-full h-auto flex flex-col justify-center items-center">
+                    <x-filament::icon
+                        icon="heroicon-o-photo"
+                        class="text-gray-700 h-32 w-32"
+                    />
+                    <span class="text-gray-700">
+                        Tidak ada background untuk jum'at.
+                    </span>
+                </div>
+            @else
+                @foreach ($this->getBackgrounds(true) as $background)
+                    <x-filament::grid.column>
+                        <img 
+                            src="{{ asset('storage/'.$background->image_path) }}" 
+                            alt="{{ $background->name }}"
+                            wire:click="openViewModal({{ $background->id }})"
+                        >
+                    </x-filament::grid.column>
+                @endforeach
+            @endif
+        </x-filament::grid>
+    </x-filament::section>
+
+    @php
+        $background = $this->findBackground($this->selectedId);
+    @endphp
+    <x-filament::modal
+        id="view-modal"
+        width="{{ $this->viewModalWidth }}"
+    >
+        <x-slot name="heading">
+            View ({{ $this->selectedId !== null ? basename($background->image_path) : null }})
+        </x-slot>
+
+        <div class="w-full flex flex-col gap-4">
+            <div class="w-full flex flex-row-reverse">
+                {{ $this->deleteAction() }}
+            </div>
+            @if ($this->selectedId !== null)
+                <img 
+                    src="{{ asset('storage/'.$background->image_path) }}" 
+                    alt="{{ $background->name }}"
+                    class="w-full"
+                >
+            @endif
+        </div>
+    </x-filament::modal>
 
     <x-filament-actions::modals/>
 </x-filament-panels::page>
