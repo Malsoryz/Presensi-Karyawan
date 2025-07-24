9f2be224dd4d39bc631f4b18770a2a277727b2f1
Malsoryz <ikmalalansory@gmail.com>
2025-07-24 16:50:53 +0800

    update Command

--- app/Console/Commands/UpdateWorkday.php
+++ app/Console/Commands/UpdateWorkday.php
@@ -30,6 +30,32 @@ class UpdateWorkday extends Command
     public function handle()
     {
         $getYear = now()->year;
+
+        if (!HariLibur::exists()) {
+            $url = 'https://api-harilibur.vercel.app/api';
+
+            $response = Http::get($url, ['year' => $getYear])->json();
+
+            $holidaylist = array_filter($response, function ($res) {
+                return $res['is_national_holiday'] === true;
+            });
+
+            $this->info('Deleting all holiday list.');
+
+            HariLibur::truncate();
+
+            $this->info('Adding holiday list this year');
+
+            foreach ($holidaylist as $holiday) {
+                HariLibur::create([
+                    'nama' => $holiday['holiday_name'],
+                    'tanggal' => $holiday['holiday_date'],
+                    'bulan' => Carbon::parse($holiday['holiday_date'])->month,
+                ]);
+            }
+
+            $this->info('Successfully add new holiday list.');
+        }
         
         $this->info('Deleting all workday list.');
 
