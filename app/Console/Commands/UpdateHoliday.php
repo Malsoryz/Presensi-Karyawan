045c6a3827c539ff3a4d6ba3093ce84e36aacd6c
Malsoryz <ikmalalansory@gmail.com>
2025-07-24 14:48:59 +0800

    separating holiday and workday update command

--- app/Console/Commands/UpdateHoliday.php
+++ app/Console/Commands/UpdateHoliday.php
@@ -5,7 +5,6 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 use App\Models\HariLibur;
-use App\Models\HariKerja;
 use Carbon\Carbon;
 
 class UpdateHoliday extends Command
@@ -22,7 +21,7 @@ class UpdateHoliday extends Command
      *
      * @var string
      */
-    protected $description = 'Memperbarui data hari kerja.';
+    protected $description = 'Memperbarui data hari libur.';
 
     /**
      * Execute the console command.
@@ -53,54 +52,5 @@ class UpdateHoliday extends Command
         }
 
         $this->info('Successfully add new holiday list.');
-
-        $this->info('Deleting all workday list.');
-
-        HariKerja::truncate();
-
-        $getHolidayLists = HariLibur::whereYear('tanggal', $getYear)
-            ->pluck('tanggal')
-            ->map(fn($t) => Carbon::parse($t)->toDateString())
-            ->toArray();
-
-        for ($month=1; $month <= 12; $month++) { 
-            $totalDay = Carbon::create($getYear, $month)->daysInMonth;
-            $hariMinggu = 0;
-            $hariLibur = 0;
-
-            for ($day=0; $day <= $totalDay; $day++) { 
-                $tanggal = Carbon::create($getYear, $month, $day);
-                $strTanggal = $tanggal->toDateString();
-
-                if ($tanggal->isSunday()) {
-                    $hariMinggu++;
-                }
-
-                if (in_array($strTanggal, $getHolidayLists)) {
-                    $hariLibur++;
-                }
-            }
-
-            $totalLibur = $hariMinggu + $hariLibur;
-            $totalKerja = $totalDay - $totalLibur;
-            $bulanNama = Carbon::createFromDate(null, $month, null)->translatedFormat('F');
-
-            $this->info($bulanNama);
-            $this->info("Total day: $totalDay");
-            $this->info("Total sunday: $hariMinggu");
-            $this->info("Total national holiday: $hariLibur");
-            $this->info("Total holiday: $totalLibur");
-            $this->info("Total workday: $totalKerja");
-
-            HariKerja::insert([
-                'bulan' => $month,
-                'total_hari' => $totalDay,
-                'total_hari_minggu' => $hariMinggu,
-                'total_hari_libur_nasional' => $hariLibur,
-                'total_hari_libur' => $totalLibur,
-                'total_hari_kerja' => $totalKerja
-            ]);
-        }
-        $this->info('Successfully add new workday list.');
     }
 }
---
+++ app/Console/Commands/UpdateWorkday.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+use App\Models\HariLibur;
+use App\Models\HariKerja;
+use Carbon\Carbon;
+
+class UpdateWorkday extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'workday:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Memperbarui data hari kerja';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $getYear = now()->year;
+        
+        $this->info('Deleting all workday list.');
+
+        HariKerja::truncate();
+
+        $getHolidayLists = HariLibur::whereYear('tanggal', $getYear)
+            ->pluck('tanggal')
+            ->map(fn($t) => Carbon::parse($t)->toDateString())
+            ->toArray();
+
+        for ($month=1; $month <= 12; $month++) { 
+            $totalDay = Carbon::create($getYear, $month)->daysInMonth;
+            $hariMinggu = 0;
+            $hariLibur = 0;
+
+            for ($day=0; $day <= $totalDay; $day++) { 
+                $tanggal = Carbon::create($getYear, $month, $day);
+                $strTanggal = $tanggal->toDateString();
+
+                if ($tanggal->isSunday()) {
+                    $hariMinggu++;
+                }
+
+                if (in_array($strTanggal, $getHolidayLists)) {
+                    $hariLibur++;
+                }
+            }
+
+            $totalLibur = $hariMinggu + $hariLibur;
+            $totalKerja = $totalDay - $totalLibur;
+            $bulanNama = Carbon::createFromDate(null, $month, null)->translatedFormat('F');
+
+            $this->info($bulanNama);
+            $this->info("Total day: $totalDay");
+            $this->info("Total sunday: $hariMinggu");
+            $this->info("Total national holiday: $hariLibur");
+            $this->info("Total holiday: $totalLibur");
+            $this->info("Total workday: $totalKerja");
+
+            HariKerja::insert([
+                'bulan' => $month,
+                'total_hari' => $totalDay,
+                'total_hari_minggu' => $hariMinggu,
+                'total_hari_libur_nasional' => $hariLibur,
+                'total_hari_libur' => $totalLibur,
+                'total_hari_kerja' => $totalKerja
+            ]);
+        }
+        $this->info('Successfully add new workday list.');
+    }
+}
