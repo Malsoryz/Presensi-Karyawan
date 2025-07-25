c26963ac16051135f521c7a11d811f2ed596f1d1
Malsoryz <ikmalalansory@gmail.com>
2025-07-25 15:07:31 +0800

    Add some new method

--- app/Models/HariLibur.php
+++ app/Models/HariLibur.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use App\Models\Config as Cfg;
 
 class HariLibur extends Model
 {
@@ -18,9 +19,19 @@ class HariLibur extends Model
         'bulan',
     ];
 
-    public static function getInMonth(string|int $month)
+    // public static function getInMonth(string|int $month)
+    // {
+    //     return self::select('tanggal')->whereMonth('tanggal', $month)->get();
+    // }
+
+    public static function isHoliday()
     {
-        return self::select('tanggal')->whereMonth('tanggal', $month)->get();
+        $today = now(Cfg::timezone())->toDateString();
+        return self::whereDate('tanggal', $today)->exists();
     }
 
+    public static function isSunday()
+    {
+        return now(Cfg::timezone())->isSunday();
+    }
 }
