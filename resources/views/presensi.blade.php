94a0e370abb68a78940f5ae0267354e57d791bcf
Malsoryz <ikmalalansory@gmail.com>
2025-07-25 17:00:48 +0800

    using prefix for route and use presensi.blade.php as main layout

--- resources/views/presensi.blade.php
+++ resources/views/presensi.blade.php
@@ -1,4 +1,4 @@
-<x-presensi title="Presensi">
+<x-layout title="Presensi">
     <x-slot name="header">
         <x-presensi.header/>
     </x-slot>
@@ -21,12 +21,25 @@
                 <div class="flex flex-col gap-4">
                     <div class="card glassmorphism w-72 h-32">
                         {{-- Jam dinamis --}}
+                        @if (session('info'))
+                            {{ session('info') }}
+                        @endif
                     </div>
                     <div class="card glassmorphism w-72 h-72">
-                        {{-- QRcode --}}
+                        <div 
+                            x-data="svgRefresh()" 
+                            x-init="start()" 
+                            x-html="svg"
+                        ></div>
                     </div>
-                    <div class="card glassmorphism w-72 h-32">
+                    <div 
+                        class="card glassmorphism w-72 h-32"
+                        x-data="userData()"
+                        x-init="start()"
+                        x-text="message"
+                    >
                         {{-- Status --}}
+
                     </div>
                 </div>
                 <div class="card glassmorphism w-128">
@@ -36,7 +49,65 @@
         </div>
     </main>
 
-</x-presensi>
+<x-slot name="scriptAfter">
+<script>
+    function userData() {
+        return {
+            message: '',
+            isDetected: false,
+            intervalId: null,
+            start() {
+                this.getData();
+                if (!this.intervalId && !this.isDetected) {
+                    this.intervalId = setInterval(() => {
+                        console.log('mendapatkan ulang data');
+                        this.getData();
+
+                        if (this.isDetected) {
+                            clearInterval(this.intervalId);
+                            this.intervalId = null;
+                        }
+                    }, 3000);
+                }
+            },
+            getData() {
+                axios.get("{{ route('presensi.get-user') }}")
+                .then(res => {
+                        this.message = res.data.message;
+                        this.isDetected = res.data.is_detected;
+                        console.log(this.message);
+                        console.log('user terdeteksi: ' + this.isDetected);
+                    });
+            }
+        }
+    }
+
+    function svgRefresh() {
+        return {
+            svg: '',
+            start() {
+                console.log('Memulai membuat qr code baru');
+                this.loadSvg();
+                setInterval(() => {
+                    console.log('membuat qr code baru');
+                    this.loadSvg();
+                }, 60000);
+            },
+            loadSvg() {
+                axios.get("{{ route('presensi.getqr') }}")
+                    .then(res => {
+                        this.svg = res.data;
+                    })
+                    .catch(err => {
+                        console.error('Gagal memuat svg: ', err);
+                    });
+            }
+        }
+    }
+</script>
+</x-slot>
+
+</x-layout>
 
 {{-- <div class="flex flex-col gap-4">
     <div class="card glassmorphism w-128 h-64">
--- routes/web.php
+++ routes/web.php
@@ -14,23 +14,38 @@ Route::redirect('/', '/presensi');
 //     ->middleware(['auth', 'verified'])
 //     ->name('dashboard');
 
-Route::middleware(['auth'])->group(function () {
+// Route::middleware(['auth'])->group(function () {
     // Route::redirect('settings', 'settings/profile');
 
     // Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
     // Volt::route('settings/password', 'settings.password')->name('settings.password');
     // Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 
-    Route::get('/presensi', [PresensiControllers::class, 'index'])
-        ->name('presensi.index');
-    Route::get('/presensi/store/{name}/{token}', [PresensiControllers::class, 'store'])
-        ->name('presensi.store');
-    Route::get('/presensi/scan-check', [PresensiControllers::class, 'scanCheck'])
-        ->name('presensi.scanCheck');
-});
+    // Route::get('/presensi', [PresensiControllers::class, 'index'])
+    //     ->name('presensi.index');
+    // Route::get('/presensi/store/{name}/{token}', [PresensiControllers::class, 'store'])
+    //     ->name('presensi.store');
+    // Route::get('/presensi/scan-check', [PresensiControllers::class, 'scanCheck'])
+    //     ->name('presensi.scanCheck');
+// });
 
-Route::get('/test', function () {
-    return view('presensi');
+Route::prefix('presensi')->controller(PresensiControllers::class)->group(function () {
+    Route::get('/', 'index')
+        ->name('presensi.index');
+    Route::get('/store/{token}', 'store')
+        ->name('presensi.store')
+        ->middleware('auth');
+    Route::get('/get-qr', 'getQr')
+        ->name('presensi.getqr');
+        Route::get('/get-user', 'getUser')
+        ->name('presensi.get-user');
+        
+    Route::get('/check-cookie', 'checkCookie')
+        ->name('presensi.check-cookie');
+
+    Route::get('/test', function () {
+        return auth()->user()->name;
+    });
 });
 
 require __DIR__.'/auth.php';
